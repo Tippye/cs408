839d663f874674999096a2ad5d0c3ab590adb747
Tippy <tippy_q@163.com>
2022-10-14 16:24:56 +0800

    修复kmp算法中换行导致的代码错误

--- 4.String/2.2KMP算法.cpp
+++ 4.String/2.2KMP算法.cpp
@@ -1,27 +1,26 @@
 //
 // Created by 渠继旺 on 2022/10/14.
 //
-#define
-Maxsize 255
+#define Maxsize 255
 
 typedef struct {
-char str[Maxsize];
-int len;
+    char str[Maxsize];
+    int len;
 } SString;
 
 // 匹配 T 在 S 中的位置
 int Index_KMP(SString S, SString T, int next[]) {
-int i, j;
-i = j = 1;
-while (i <= S.len && j <= T.len) {
-if (j == 0 || S.str[i] == T.str[j]) {
-++i;
-++j;
-} else {
-j = next[j];
-}
-}
-if (j > T.len)
-return i - T.len;
-return 0;
+    int i, j;
+    i = j = 1;
+    while (i <= S.len && j <= T.len) {
+        if (j == 0 || S.str[i] == T.str[j]) {
+            ++i;
+            ++j;
+        } else {
+            j = next[j];
+        }
+    }
+    if (j > T.len)
+        return i - T.len;
+    return 0;
 }
