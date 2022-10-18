467ec27809eff0804f8933cf78f865c19f46cf9e
Tippy <tippy_q@163.com>
2022-10-18 12:38:03 +0800

    修改线索二叉树代码

--- 5.Tree/3.2线索二叉树.cpp
+++ 5.Tree/3.2线索二叉树.cpp
@@ -40,6 +40,7 @@ void InThread(ThreadTree T) {
 }
 
 //创建中序线索化二叉树T
+//这里的 T 不是引用是因为 T 的内容在函数中没有改变，只改变了内容所指向的地址的内容
 void CreatInThread(ThreadTree T) {
     pre = NULL;
     if (T != NULL) {//非空二叉树才能线索化
@@ -50,38 +51,14 @@ void CreatInThread(ThreadTree T) {
     }
 }
 
-//中序线索化（王道教材版）
-void InThread_CSKaoYan(ThreadTree p, ThreadTree &pre) {
-    if (p != NULL) {
-        InThread_CSKaoYan(p->lchild, pre);//递归，线索化左子树
-        if (p->lchild == NULL) {//左子树为空，建立前驱线索
-            p->lchild = pre;
-            p->ltag = 1;
-        }
-        if (pre != NULL && pre->rchild == NULL) {
-            pre->rchild == p;//建立前驱结点的后及线索
-            pre->rtag = 1;
-        }
-        pre = p;
-        InThread_CSKaoYan(p->rchild, pre);
-    }
-}
-
-//中序线索化二叉树（王道教材版本）
-void CreatInThread_CSKaoYan(ThreadTree T) {
-    ThreadTree pre = NULL;
-    if (T != NULL) {
-        InThread_CSKaoYan(T, pre);
-        pre->rchild = NULL;//思考：为什么处理最后一个结点时没有判断rchild 是否为NULL？
-        pre->rtag = 1;//答：因为最后一个结点的右孩子必为空。
-    }
-}
+////以上为中序线索二叉树的构建
 
 //先序线索化，一边遍历一边线索化
 void PreThread(ThreadTree T) {
     if (T != NULL) {
         visit(T);
-        if (0 == T->ltag) {//lchild不是前驱线索
+        if (T->ltag == 0) {//lchild不是前驱线索
+            // 这里如果不判断会进入死循环，因为刚刚线索化的左指针肯定不会NULL
             PreThread(T->lchild);
         }
         PreThread(T->rchild);
@@ -99,34 +76,8 @@ void CreatPreThread(ThreadTree T) {
     }
 }
 
-//先序线索化（王道教程版本）
-void PreThread_CSKaoYan(ThreadTree p, ThreadTree &pre) {
-    if (p != NULL) {
-        if (p->lchild == NULL) {
-            p->lchild = pre;
-            p->ltag = 1;
-        }
-        if (pre != NULL && pre->rchild == NULL) {
-            pre->rchild == p;//建立前驱结点的后及线索
-            pre->rtag = 1;
-        }
-        pre = p;
-        if (0 == p->ltag) {
-            PreThread_CSKaoYan(p->lchild, pre);
-        }
-        PreThread_CSKaoYan(p->rchild, pre);
-    }
-}
+////以上为先序线索二叉树的构建
 
-//先序线索化二叉树（王道教材版本）
-void CreatPreThread_CSKaoYan(ThreadTree T) {
-    ThreadTree pre = NULL;
-    if (T != NULL) {
-        PreThread_CSKaoYan(T, pre);
-        if (pre->rchild = NULL)//处理遍历的最后一个结点
-            pre->rtag = 1;
-    }
-}
 
 //后序线索二叉树
 void PostThread(ThreadTree T) {
@@ -148,32 +99,8 @@ void CreatPostThread(ThreadTree T) {
     }
 }
 
-//后序线索化（王道教程版本）
-void PostThread_CSKaoYan(ThreadTree p, ThreadTree &pre) {
-    if (p != NULL) {
-        PostThread_CSKaoYan(p->lchild, pre);
-        PostThread_CSKaoYan(p->rchild, pre);
-        if (p->lchild == NULL) {
-            p->lchild = pre;
-            p->ltag = 1;
-        }
-        if (pre != NULL && pre->rchild == NULL) {
-            pre->rchild == p;//建立前驱结点的后及线索
-            pre->rtag = 1;
-        }
-        pre = p;
-    }
-}
+////以上为后序线索二叉树的构建
 
-//后序线索化二叉树（王道教材版本）
-void CreatPostThread_CSKaoYan(ThreadTree T) {
-    ThreadTree pre = NULL;
-    if (T != NULL) {
-        PostThread_CSKaoYan(T, pre);
-        if (pre->rchild = NULL)//处理遍历的最后一个结点
-            pre->rtag = 1;
-    }
-}
 
 //中序线索二叉树找中序后继
 //找到以P为根的子树中，第一个被中序遍历的结点
@@ -203,15 +130,17 @@ void InOrder(ThreadNode *T) {
 //找到以p为根的子树中，最后一个被中序遍历的结点
 ThreadNode *LastNode(ThreadNode *p) {
     //循环找到最右下结点（不一定是叶结点）
-    while (0 == p->rtag)p = p->rchild;
+    while (0 == p->rtag) p = p->rchild;
     return p;
 }
 
 //在中序线索二叉树中找到结点p的前驱结点
 ThreadNode *PreNode(ThreadNode *p) {
     //左下子树中最右结点
-    if (0 == p->ltag)return LastNode(p->lchild);
-    else return p->lchild;
+    if (0 == p->ltag)
+        return LastNode(p->lchild);
+    else
+        return p->lchild;
 }
 
 //对中序线索二叉树进行逆向中序遍历
