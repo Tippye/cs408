6946100bb4dbdc05428ef8c2d3545bfd625a3a3f
Tippy <tippy_q@163.com>
2023-03-04 11:54:44 +0800

    增加比较与应用部分笔记和错题，插入排序和冒泡排序增加了比较次数计数

--- DS/8.Sort/冒泡排序.h
+++ DS/8.Sort/冒泡排序.h
@@ -7,6 +7,7 @@
 #define ElemType int
 
 void BubbleSort(ElemType A[], int n) {
+    int num = 0;
     bool flag;                                  // 用来记录这一趟有没有更改元素位置
     for (int i = 0; i < n - 1; ++i) {
         flag = false;
@@ -15,6 +16,8 @@ void BubbleSort(ElemType A[], int n) {
                 swap(A[j - 1], A[j]);    // 交换两个数的位置
                 flag = true;
             }
+            num++;
+            printf("第%d次，比较%d和%d\n", num, A[j - 1], A[j]);
         }
 //        for (int j = 0; j < n; ++j) {           // 可以解开注释看到每一趟后的结果
 //            printf("%d, ",A[j]);
@@ -25,9 +28,14 @@ void BubbleSort(ElemType A[], int n) {
 }
 
 void TestModule() {
-    ElemType list[] = {0,38,65,22,76,13,27,49};
-    BubbleSort(list,7);
-    for (int i=1;i<7;i++){
-        printf("%d,",list[i]);
+//    ElemType list[] = {0,38,65,22,76,13,27,49};
+//    BubbleSort(list,7);
+//    for (int i=1;i<7;i++){
+//        printf("%d,",list[i]);
+//    }
+    ElemType list[] = {0, 1, 2, 3, 4, 5, 10, 6, 7, 8, 9};
+    BubbleSort(list, 10);
+    for (int i = 1; i < 10; i++) {
+        printf("%d,", list[i]);
     }
 }
--- DS/8.Sort/插入排序.h
+++ DS/8.Sort/插入排序.h
@@ -4,22 +4,32 @@
 #define ElemType int
 
 void InsertSort(ElemType A[], int n){
+    int num=0;
     int i, j;
     ElemType temp;
     for(i=1;i<=n;i++){                      // 循环无序列表
         if(A[i]<A[i-1]){                    // 如果这一个关键字比前边的小，就把它插入到前边
             temp = A[i];                    // 先把待插入的元素存放到0号位置
             for (j=i-1;temp<A[j];--j)       // 从i-1号位置往前找插入位置
+                num++;
+                printf("第%d次 ，比较%d和%d\n", num, temp, A[j]);
                 A[j+1] = A[j];              // i-1号位置到插入位置之间的元素都往后移动一位
             A[j+1]=temp;                    // 把待插入元素插入到相应位置
         }
+        num++;
+        printf("第%d次，比较%d和%d\n", num, A[i], A[i - 1]);
     }
 }
 
 void TestModule() {
-    ElemType list[] = {0,38,65,22,76,13,27,49};
-    InsertSort(list,7);
-    for (int i=1;i<7;i++){
-        printf("%d,",list[i]);
+//    ElemType list[] = {0,38,65,22,76,13,27,49};
+//    InsertSort(list,7);
+//    for (int i=1;i<7;i++){
+//        printf("%d,",list[i]);
+//    }
+    ElemType list[] = {0, 1, 2, 3, 4, 5, 10, 6, 7, 8, 9};
+    InsertSort(list, 10);
+    for (int i = 1; i < 10; i++) {
+        printf("%d,", list[i]);
     }
 }
