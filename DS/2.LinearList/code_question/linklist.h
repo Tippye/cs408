28f67a82408b2202a686a936db2f28040d734575
Tippy <tippy_q@163.com>
2023-03-19 11:18:50 +0800

    提交链表代码使用的头文件

--- DS/2.LinearList/code_question/linklist.h
+++ DS/2.LinearList/code_question/linklist.h
@@ -1,8 +1,26 @@
 //
 // Created by 渠继旺 on 2023/3/18.
 //
+// 此文件是链表公用头文件，定义了基础链表和打印链表的方法，结构同LeetCode链表结点
+//
+#include <iostream>
+#include <vector>
+using namespace std;
 
-#ifndef DATASTRUCTURE_LINKLIST_H
-#define DATASTRUCTURE_LINKLIST_H
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-#endif //DATASTRUCTURE_LINKLIST_H
+ostream& operator<<(ostream& os, ListNode* lp) {
+    ListNode* cur = lp;
+    while (cur != nullptr) {
+        os << cur->val << " -> ";
+        cur = cur->next;
+    }
+    os << "∅";
+    return os << endl;
+}
